7e54d5acab3cdcb2a24370b108c312f46cba0d24
tresvitae <tresvitae@hotmail.com>
2018-05-04 00:01:36 +0200

    visual polish of code

--- app/src/main/java/com/example/android/lublintourguide/MainActivity.java
+++ app/src/main/java/com/example/android/lublintourguide/MainActivity.java
@@ -57,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
-
                         return true;
                     }
                 });
